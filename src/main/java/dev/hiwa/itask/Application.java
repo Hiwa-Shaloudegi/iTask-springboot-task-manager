5bf7b96d30dbf84125d62681bf917cdbdac8470b
Hiwa Shalouedgi <hiwa.shaloudegi@gmail.com>
2025-06-30 13:38:06 +0330

    init commit

---
+++ src/main/java/dev/hiwa/itask/Application.java
@@ -0,0 +1,13 @@
+package dev.hiwa.itask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
